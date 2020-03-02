260c8d39f4fc7a0dc447e6d9abe4709b8ed82891
Aleksey <cs-zatopija@mail.ru>
2020-03-02 05:32:11 +0300

    feat: add solution

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,16 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (Array.isArray(members) == false) return false;
+  
+  let team = [];
+
+  for (let item of members) {
+    if (typeof item == 'string') {
+
+      let str = item.trim();
+      team.push(str[0].toUpperCase());
+
+    }
+  };
+
+  return team.sort().join('');
 };
--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,26 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater( str, options ) {
+    let separator = options.separator ? options.separator : '+';
+    let repeatTimes = options.repeatTimes ? options.repeatTimes : 1;
+    let additionRepeatTimes = options.additionRepeatTimes ? options.additionRepeatTimes : 1;
+    let additionalSeporator = options.additionSeparator ? options.additionSeparator : '|';
+    let arrayMain = [];
+    let addition;
+
+    if (typeof(options.addition) == "undefined") {
+        addition = '';
+    } else {
+        addition = options.addition  + '';
+    }
+    
+    for (let i=0; i < repeatTimes; i+=1) {
+
+        let seporator = [];
+
+        for (j=0; j<additionRepeatTimes; j+=1) {
+            seporator.push(addition);
+        }
+
+        arrayMain.push(str+seporator.join(additionalSeporator));
+    }
+    return(arrayMain.join(separator));
 };
-  
